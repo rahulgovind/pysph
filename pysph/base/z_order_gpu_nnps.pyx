75ea94b69f267d101dae4d62595be819dd16b828
ragnar <rahulgovind517@gmail.com>
2018-04-01 18:38:39 +0530

    Fix bug with zorder profiling

--- pysph/base/z_order_gpu_nnps.pyx
+++ pysph/base/z_order_gpu_nnps.pyx
@@ -22,7 +22,7 @@ cimport numpy as np
 from mako.template import Template
 
 from pysph.base.gpu_nnps_helper import GPUNNPSHelper
-from pysph.base.opencl import DeviceArray, get_config, profile
+from pysph.base.opencl import DeviceArray, get_config, profile_kernel
 
 
 IF UNAME_SYSNAME == "Windows":
@@ -111,12 +111,11 @@ cdef class ZOrderGPUNNPS(GPUNNPS):
                 scan_kernel=GenericScanKernel, key_expr="keys[i]",
                 sort_arg_names=["pids", "keys"]
             )
-
+            self.radix_sort = profile_kernel(self.radix_sort, 'radix_sort')
 
         (sorted_indices, sorted_keys), evnt = self.radix_sort(
             self.pids[pa_index].array, self.pid_keys[pa_index].array, key_bits=64
         )
-        profile("radix_sort", evnt)
         self.pids[pa_index].set_data(sorted_indices)
         self.pid_keys[pa_index].set_data(sorted_keys)
 
